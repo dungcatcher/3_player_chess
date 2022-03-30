3406247ca388a30bda4c410734ec0a4fb5e96430
OggyP <oggyp@oggyp.com>
2022-03-30 20:28:16 +1100

    fixed cross segment moves, cleaned up vector_to_position

--- movegen.py
+++ movegen.py
@@ -1,8 +1,11 @@
 """TODO: Make vector_to_position code not garbage"""
+import re
+from sys import api_version
+from typing import List, Union
 from position import *
 
 
-def direction_to_square(position, vector):
+def direction_to_square(position: Position, vector):
     new_square = position.square + Vector2(vector)
     new_segment = position.segment
     if 0 <= new_square.y <= 3:
@@ -12,47 +15,55 @@ def direction_to_square(position, vector):
             return None
     else:
         if new_square.y < 0:  # Goes above the segment
-            if 0 <= new_square.x <= 3:
+            if 0 <= new_square.x <= 3:  # Goes to the left upper segment
                 new_segment = (position.segment - 1) % 3
                 new_square = [7 - position.square.x - vector[0],
-                              3 - (position.square.y - vector[1])]
+                              abs(new_square.y) - 1]
                 return Position(new_segment, new_square)
-            elif 4 <= new_square.x <= 7:
+            elif 4 <= new_square.x <= 7:  # Goes to the right upper segment
                 new_segment = (position.segment + 1) % 3
                 new_square = [7 - position.square.x - vector[0],
-                              3 - (position.square.y - vector[1])]
+                              abs(new_square.y) - 1]
                 return Position(new_segment, new_square)
+            # Goes past the end of the segment to the right or left (off the board)
             else:
                 return None
         else:  # Below the segment (off the board)
             return None
 
 
-def vector_to_position(position, vector):
-    xy_first_position = direction_to_square(position, [vector[0], 0])
-    xy_final_position: None | Position = None
-    if xy_first_position:
-        xy_final_position = direction_to_square(
-            xy_first_position, [0, vector[1]])
+def positions_are_same(p1: Position, p2: Position):
+    if p1 and p2\
+            and p1.segment == p2.segment\
+            and p1.square.x == p2.square.x\
+            and p1.square.y == p2.square.y:
+        return True
+    return False
 
-    yx_first_position = direction_to_square(position, [0, vector[1]])
-    yx_final_position = None
-    if yx_first_position:
-        if yx_first_position.segment != position.segment:  # Direction changes with new segment
-            yx_final_position = direction_to_square(
-                yx_first_position, [-vector[0], 0])
-        else:
-            yx_final_position = direction_to_square(
-                yx_first_position, [vector[0], 0])
 
-    if xy_final_position is not None and yx_final_position is not None \
-            and xy_final_position.segment == yx_final_position.segment and xy_final_position.square == yx_final_position.square:
-        positions = [xy_final_position]
+def vector_to_position(position: Position, vector: List[float]):
+    first_pos: List[Union[Position, None]] = [None, None]
+    end_pos: List[Union[Position, None]] = [None, None]
+
+    for i in range(2):
+        vec_to_check = [0, 0]
+        vec_to_check[i] = vector[i]
+        first_pos[i] = (direction_to_square(position, vec_to_check))
+
+        if first_pos[i]:
+            vec_to_check = [0, 0]
+            second_index = (i + 1) % 2
+            if position.segment == first_pos[i].segment:
+                vec_to_check[second_index] = vector[second_index]
+            else:
+                vec_to_check[second_index] = -vector[second_index]
+            end_pos[i] = (direction_to_square(
+                first_pos[i], vec_to_check))
+
+    if positions_are_same(end_pos[0], end_pos[1]):
+        return [end_pos[0]]
     else:
-        positions = [xy_final_position, yx_final_position]
-
-    return positions
-
+        return end_pos
 
 colour_to_segment = {
     "w": 0,
@@ -95,7 +106,8 @@ def pawn_movegen(board, position, colour):
 
 def knight_movegen(board, position, colour):
     moves = []
-    vectors = [[-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1]]
+    vectors = [[-1, -2], [1, -2], [2, -1], [2, 1],
+               [1, 2], [-1, 2], [-2, 1], [-2, -1]]
 
     for vector in vectors:
         for position_to_check in vector_to_position(position, vector):
@@ -107,7 +119,6 @@ def knight_movegen(board, position, colour):
                 else:
                     if square_occupant[0] != colour:
                         moves.append(position_to_check)
-
     return moves
 
 
@@ -129,7 +140,8 @@ def iterate_move(board, position, vector, colour):
                 position_to_check.square.y)][int(position_to_check.square.x)]
             if square_occupant is None:
                 moves.append(position_to_check)
-                moves += iterate_move(board, position_to_check, vector if position_to_check.segment == position.segment else [-vector[0], -vector[1]], colour)
+                moves += iterate_move(board, position_to_check, vector if position_to_check.segment ==
+                                      position.segment else [-vector[0], -vector[1]], colour)
             elif square_occupant[0] != colour:
                 moves.append(position_to_check)
     return moves
@@ -166,14 +178,16 @@ def rook_movegen(board, position, colour):
 
 
 def queen_movegen(board, position, colour):
-    moves = bishop_movegen(board, position, colour) + rook_movegen(board, position, colour)
+    moves = bishop_movegen(board, position, colour) + \
+        rook_movegen(board, position, colour)
 
     return moves
 
 
 def king_movegen(board, position, colour):
     moves = []
-    vectors = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]
+    vectors = [[-1, -1], [0, -1], [1, -1],
+               [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]
 
     for vector in vectors:
         for position_to_check in vector_to_position(position, vector):
--- polygons.py
+++ polygons.py
@@ -1,4 +1,7 @@
 import math
+from position import *
+from board import *
+from shapely.geometry import Point, Polygon
 
 # half_segment_polygon = [  # Entire polygon for half segment 1
 #     [587, 72], [1092, 72], [1092, 946], [335, 509]
@@ -23,15 +26,25 @@ def rotate(origin, point, angle):
     return [qx, qy]
 
 
+def position_to_pixel(position: Position):
+    polygon_points = board_polygons[int(
+        position.segment)][int(position.square.y)][int(position.square.x)]
+    polygon = Polygon(polygon_points)
+    pixel_pos = polygon.centroid.coords[:][0]
+    return pixel_pos
+
+
 def compute_polygons():
     all_polygons = []
     for segment in range(3):
-        segment_polygons = [[[] for x in range(8)] for y in range(4)]  # 8x4 array of polygons for the segment
+        # 8x4 array of polygons for the segment
+        segment_polygons = [[[] for x in range(8)] for y in range(4)]
         for half_segment in range(2):
             angle = -math.radians((2 * segment + half_segment) * 60)
             rotated_half_segment_polygon = []
             for point in half_segment_polygon:
-                rotated_half_segment_polygon.append(rotate(half_segment_polygon[1], point, angle))
+                rotated_half_segment_polygon.append(
+                    rotate(half_segment_polygon[1], point, angle))
 
             for y in range(4):  # Top left corners of polygons
                 for x in range(4):
@@ -41,12 +54,15 @@ def compute_polygons():
                             if i == 1:  # Points go anti-clockwise
                                 j = 1 if j == 0 else 0
                             top_lerp = lerp_vector(rotated_half_segment_polygon[0 - 3 * half_segment],
-                                                   rotated_half_segment_polygon[1 - 3 * half_segment],
+                                                   rotated_half_segment_polygon[1 -
+                                                                                3 * half_segment],
                                                    (x + i) * 0.25)
                             bottom_lerp = lerp_vector(rotated_half_segment_polygon[3 - 3 * half_segment],
-                                                      rotated_half_segment_polygon[2 - 3 * half_segment],
+                                                      rotated_half_segment_polygon[2 -
+                                                                                   3 * half_segment],
                                                       (x + i) * 0.25)
-                            lerp_point = lerp_vector(top_lerp, bottom_lerp, (y + j) * 0.25)
+                            lerp_point = lerp_vector(
+                                top_lerp, bottom_lerp, (y + j) * 0.25)
                             polygon.append(lerp_point)
                     segment_polygons[y][x + 4 * half_segment] = polygon
         all_polygons.append(segment_polygons)
@@ -55,13 +71,15 @@ def compute_polygons():
 
 
 def handle_polygon_resize(polygons, new_scale, margin):
-    polygons_copy = [[[[] for x in range(8)] for y in range(4)] for s in range(3)]
+    polygons_copy = [[[[] for x in range(8)]
+                      for y in range(4)] for s in range(3)]
     for segment in range(3):
         for y in range(4):
             for x in range(8):
                 new_point = []
                 for point in polygons[segment][y][x]:
-                    new_point.append((point[0] * new_scale + margin, point[1] * new_scale))
+                    new_point.append(
+                        (point[0] * new_scale + margin, point[1] * new_scale))
                 polygons_copy[segment][y][x] = new_point
 
     return polygons_copy
